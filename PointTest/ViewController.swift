1eeee9012882640a1ed038003637b9f30ca92509
Justin R. Miller <incanus@codesorcery.net>
2014-12-01 10:13:12 -0800

    initial version

--- PointTest/ViewController.swift
+++ PointTest/ViewController.swift
@@ -1,25 +1,36 @@
-//
-//  ViewController.swift
-//  PointTest
-//
-//  Created by Justin R. Miller on 12/1/14.
-//  Copyright (c) 2014 Mapbox. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let map = MKMapView(frame: view.bounds)
+        map.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        view.addSubview(map)
 
+        var annotations = NSMutableArray()
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let pointData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("3000", ofType: "geojson")!)
+            let points = NSJSONSerialization.JSONObjectWithData(pointData!,
+                options: nil,
+                error: nil) as NSDictionary
+            for point in points["features"] as NSArray {
+                let coordinate = (point["geometry"] as NSDictionary)["coordinates"] as NSArray
+                let lon = coordinate[0] as CLLocationDegrees
+                let lat = coordinate[1] as CLLocationDegrees
+                let a = MKPointAnnotation()
+                a.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+                annotations.addObject(a)
+            }
+            dispatch_async(dispatch_get_main_queue()) {
+                map.addAnnotations(annotations)
+                map.showAnnotations(map.annotations, animated: false)
+            }
+        }
+
+    }
 
 }
-
